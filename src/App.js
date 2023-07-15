1eb980884a8ab6510234362fd34df122231bd31b
AhmedJabareen96 <31374628+AhmedJabareen96@users.noreply.github.com>
2023-07-16 00:47:24 +0300

    rfce

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
@@ -10,36 +10,34 @@ import TestLifeCycle from './components/Test/TestLifeCycle';
 import { Provider } from './Context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import ForgotYourPassword from "./components/ForgotPassword/ForgotYourPassword";
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import ForgotYourPassword from './components/ForgotPassword/ForgotYourPassword';
+import { UserContext } from './UserContext';
 
-
-class App extends Component {
-  render() {
-    return (
-      <Provider>
-       <BrowserRouter>
+const App = () => {
+  return (
+    <Provider>
+      <BrowserRouter>
         <div className="App">
-          <Header branding = "Contact Manager" />
+          <Header branding="Contact Manager" />
           <div className="container">
             <Routes>
-              <Route exact path='/'            element={<Contacts />} />
-              <Route exact path='/contact/add' element={<AddContact />} />
-              <Route exact path='/contact/edit/:id' element={<EditContact />} />
-              <Route exact path='/orders'       element={<About />} />
-              <Route exact path='/login'       element={<Login />} />
-              <Route exact path='/register'       element={<Register />} />
-              <Route exact path='/forgot-password'       element={<ForgotYourPassword />} />
-              <Route exact path='/test'        element={<TestLifeCycle favColor = 'yellow'/>} />
-              <Route path='*' element={<NotFound />} />
+              <Route exact path="/" element={<Contacts />} />
+              <Route exact path="/contact/add" element={<AddContact />} />
+              <Route exact path="/contact/edit/:id" element={<EditContact />} />
+              <Route exact path="/orders" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/forgot-password" element={<ForgotYourPassword />} />
+              <Route exact path="/test" element={<TestLifeCycle favColor="yellow" />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </div>
-       </BrowserRouter> 
-      </Provider>
-    );
-  }
-}
- 
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
 export default App;
