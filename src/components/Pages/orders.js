9da2ac69864e7d4d8891765a938048327e71294b
AhmedJabareen96 <31374628+AhmedJabareen96@users.noreply.github.com>
2023-07-18 02:45:42 +0300

    deiveries list fetch api

--- src/components/Pages/orders.js
+++ src/components/Pages/orders.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const About = () => {
+  const [people, setPeople] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/deliveries/');
+        setPeople(response.data);
+      } catch (error) {
+        console.log('Error fetching data from the API:', error);
+      }
+    };
 
-  const people = [
-    'Creola Katherine Johnson: mathematician',
-    'Mario José Molina-Pasquel Henríquez: chemist',
-    'Mohammad Abdus Salam: physicist',
-    'Percy Lavon Julian: chemist',
-    'Subrahmanyan Chandrasekhar: astrophysicist'
-  ];
-  const listItems = people.map(person => <li>{person}</li>);
+    fetchData();
+  }, []);
+
+  const listItems = people.map(person => <li key={person.id}>{person.name}: {person.profession}</li>);
 
   return (
     <>
-    <h1 className="display-4 mb-2" style={{ textAlign: 'center' }}>
-      <span className='text-danger'>Orders</span> List
-    </h1>
-    <ul className='list-group-item'>{listItems}</ul>
-    <ul className='list-group'>
-      <li></li>
-    </ul>
-{/* 
-    {showContactInfo ? (
-              <ul className='list-group'>
-                <li className='list-group-item'>Email : {email}</li>
-                <li className='list-group-item'>Phone : {phone}</li>
-              </ul> ): null } */}
-
+      <h1 className="display-4 mb-2" style={{ textAlign: 'center' }}>
+        <span className='text-danger'>Orders</span> List
+      </h1>
+      <ul className='list-group-item'>{listItems}</ul>
+      <ul className='list-group'>
+        <li></li>
+      </ul>
     </>
   );
 }
 
-export default About
+export default About;
