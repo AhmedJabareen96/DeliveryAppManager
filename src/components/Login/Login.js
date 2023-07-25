37dad5f528005a0983c664b34131283d5e77c177
AhmedJabareen96 <31374628+AhmedJabareen96@users.noreply.github.com>
2023-07-26 01:56:26 +0300

    login regex

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,51 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import {UserContext} from "../../UserContext";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
-    const { username, setUsername, isLoggedIn, setIsLoggedIn} = useContext(UserContext);
-    const navigate = useNavigate()
-    const [password, setPassword] = useState()
-    function handleLoginButton() {
-        axios
-    .post("http://localhost:5000/managers/loginEmail", {
-        email: username,
-        password: password
-    })
-    .then(() => {
-        console.log(`Hello ${username}`);
-        setIsLoggedIn(true); // Set isLoggedIn to true
-        navigate('/');
-    })
-    .catch(err => {
-        console.log(err);
-    });
-    }
+    const { username, setUsername, isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [password, setPassword] = useState();
+    const [errorMessage, setErrorMessage] = useState('');
 
+    function handleLoginButton() {
+        axios.post("http://localhost:5000/managers/loginEmail", {
+            email: username,
+            password: password
+        })
+        .then(() => {
+            console.log(`Hello ${username}`);
+            setIsLoggedIn(true);
+            navigate('/');
+        })
+        .catch(err => {
+            console.log(err);
+            setErrorMessage('Invalid email/password');
+        });
+    }
 
     function handleUsernameChange(event) {
-        setUsername(event.target.value)
+        setUsername(event.target.value);
     }
-    function handlePasswordChange(event){
-        setPassword(event.target.value)
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
     }
 
     return (
         <div className={styles['login-form-wrapper']}>
             <div className={styles['login-form']}>
-                <div className={styles['welcome-text']}>Welcome to Hourly Managment Console !</div>
+                <div className={styles['welcome-text']}>Welcome to Hourly Management Console!</div>
                 <TextField
                     id="loginm-username"
-                    label="Username"
+                    label="Email"
                     variant="standard"
                     className={styles['text-field']}
-                    onChange= {handleUsernameChange}
+                    onChange={handleUsernameChange}
                 />
                 <TextField
                     id="loginm-password"
@@ -51,17 +53,18 @@ const Login = () => {
                     variant="standard"
                     type="password"
                     className={styles['password-field']}
-                    onChange = {handlePasswordChange}
+                    onChange={handlePasswordChange}
                 />
-                <Button variant="contained" className={styles['button']} onClick = {handleLoginButton}>
+                <Button variant="contained" className={styles['button']} onClick={handleLoginButton}>
                     Login
                 </Button>
+                {errorMessage && <p className={styles['error-message']}>{errorMessage}</p>}
                 <p>
                     Forgot your password? <Link to="/forgot-password">Reset here</Link>
                 </p>
             </div>
         </div>
-    );
+        );
 };
 
 export default Login;
